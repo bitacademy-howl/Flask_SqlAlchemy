22dc7b96744e23b7ffe5fa1bb0d008b78d97225a
bitacademy-howl <howl1118@gmail.com>
2018-08-28 18:04:46 +0900

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+#
+# app = Flask(__name__)
+#
+#
+#
+# # app config
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql-root@localhost/travel_mate?charset=utf8'
+# app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.secret_key = 'manyrandombyte'
+#
+# db = SQLAlchemy(app)
+#
+# from app.models import *
+# # from app.routes import *
+#
+# db.create_all()
