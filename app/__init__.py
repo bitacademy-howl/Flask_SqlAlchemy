c2f02277bc8ceb4b7d072e5908c76a8175a7d133
bitacademy-howl <howl1118@gmail.com>
2018-08-29 17:56:17 +0900

    이건 ORM 테스트 다 끝나면 DAO VO 등등 모듈화 하기위해서 생각
    우선순위 낮음

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,39 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-#
-# app = Flask(__name__)
-#
-#
-#
-# # app config
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql-root@localhost/travel_mate?charset=utf8'
-# app.config['SQLALCHEMY_ECHO'] = True
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# app.secret_key = 'manyrandombyte'
-#
-# db = SQLAlchemy(app)
-#
-# from app.models import *
-# # from app.routes import *
-#
-# db.create_all()
+########################################################################################################################
+# 이건 SQLAlchemy 사용 예제 site 로 부터
+########################################################################################################################
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# flask-MYSQL 패키지 설치...후 사용 가능
+# uri 설정을 변수형태로 할 수 있다.
+from flaskext.mysql import MySQL
+
+class Music_DAO():
+
+    app = Flask(__name__)
+
+    app.config['MYSQL_DATABASE_USER'] = 'root'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'stark1234'
+    app.config['MYSQL_DATABASE_DB'] = 'webdb'
+    app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+    mysql = MySQL()
+    mysql.init_app(app)
+
+    db = SQLAlchemy(app)
+
+md = Music_DAO()
+
+
+conn = md.mysql.connect()
+cursor =conn.cursor()
+
+cursor.execute("SELECT * from customer")
+cursor.execute("INSERT INTO customer (name, gender, city) VALUE ('강수정', '여', '경기')")
+
+# cursor.
+cursor.execute("SELECT * from customer")
+data = cursor.fetchone()
+
+print(cursor.fetchall(),"\n")
--- app/models/VO.py
+++ app/models/VO.py
@@ -1,20 +1,60 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean
 
-Base = declarative_base()
+###############################################################################################################
+# 외래키 설정의 실제 데이터베이스 쿼리
+# constraint fk_id foreign key( id ) references mytable ( id ) on delete cascade
+###############################################################################################################
 
-class User(Base):
-    __tablename__ = 'users'
+class Music(Base):
+    __tablename__ = 'Music'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
+    Music_ID = Column(Integer, primary_key=True, unique=True)
+    Title = Column(String)
+    Genre = Column(String)
 
-    def __init__(self, name, fullname, password):
-        self.name = name
-        self.fullname = fullname
-        self.password = password
+    # 기타 속성들...
+    Composer = Column(String)
+    Lyricist = Column(String)
+    Hash_Tags = Column(String, nullable=True)
+
+    # Album Table
+    FK_Album_ID = Column(Integer)
+    Album = Column(String)
+
+    # Singer Table
+    FK_Singer_ID = Column(Integer)
+    Singer = Column(Integer)
+    IsGroup = Column(Boolean)
+
+    def __init__(self, Music_ID, Title, Album):
+        self.Music_ID = Music_ID
+        self.Title = Title
+        self.Album = Album
 
     def __repr__(self):
         return "<User('%s', '%s', '%s')>" % (self.name, self.fullname, self.password)
+
+
+class Album(Base):
+    __tablename__ = 'Album'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+    
+    # PK
+    Album_ID = Column(Integer)
+    
+    # 속성들
+    Album = Column(String)
+    Release_Date = Column(Integer)
+
+    # FK
+    Singer_ID = Column(Integer)
+
+class Singer(Base):
+    __tablename__ = 'Singer'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    # PK
+    Singer_ID = Column(Integer)
+    Singer = Column(String)
+    IsGroup = Column(Boolean)
